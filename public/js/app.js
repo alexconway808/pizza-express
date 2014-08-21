41fa29e42b2fccd61786ce3e4622ce57258df696
Alex Conway <alexconway808@gmail.com>
2014-08-20 21:55:59 -1000

    orders appended to the DOM

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,13 @@ $.ajax("/orders", {
   success: function(data){
     //do stuff with data
     for (var i = 0; i < data.length; i += 1){
-    console.log(data[i].toppings);  
+    //console.log(data[i].toppings);
+    var sizeOfPizza = data[i].size;
+    var typeOfMeat = data[i].toppings.meats;
+    var typeOfVeggies = data[i].toppings.veggies;
+    var pizza = sizeOfPizza + typeOfMeat + typeOfVeggies;
+    //console.log(pizza);
+    $('.order-list').append('<li>' + pizza + '</li>'); 
     }
 
   },
