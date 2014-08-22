a1d2b74adfaaa91d0a5b748d2534a0e2f1350283
Alex Conway <alexconway808@gmail.com>
2014-08-21 20:25:26 -1000

    order POSTED to console

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,43 @@
 $(document).ready(function(){
 
-$.ajax("/orders", {
-  method: "GET",
-  success: function(data){
-    //do stuff with data
-    for (var i = 0; i < data.length; i += 1){
-    //console.log(data[i].toppings);
-    var sizeOfPizza = data[i].size;
-    var typeOfMeat = data[i].toppings.meats;
-    var typeOfVeggies = data[i].toppings.veggies;
-    var pizza = sizeOfPizza + typeOfMeat + typeOfVeggies;
-    //console.log(pizza);
-    $('.order-list').append('<li>' + pizza + '</li>'); 
-    }
+  //Define the pizza order function
+  function pizzaOrderSubmitted(data){
+    console.log(data);
+      for (var i = 0; i < data.length; i += 1){
+      //console.log(data[i].toppings);
+      var sizeOfPizza = data[i].size;
+      var typeOfMeat = data[i].toppings.meats;
+      var typeOfVeggies = data[i].toppings.veggies;
+      var pizza = sizeOfPizza + typeOfMeat + typeOfVeggies;
+      //console.log(pizza);
+      $('.order-list').append('<li>' + pizza + '</li>'); 
+    };
+  };
 
-  },
-  failure: function(error){
-    //handle error
-    console.log(error);
-  }
-
-$('#form').submit(function(event){
-  //This prevents the HTML from sending to the server
-  event.preventDefault():
   $.ajax("/orders", {
-    method: "POST", 
-    data: $('#form').serialize()
-    success: function(data){
-      //do stuff with server response
-    },
+    method: "GET",
+    success: pizzaOrderSubmitted,
+      //console.log("Hi Alex");
     failure: function(error){
       //handle error
+      console.log(error);
     }
   });
-});
 
-
-});
-
+  //When form is submitted in Jade it initiates this code
+  $('#form').submit(function(event){
+    //This prevents the HTML from sending to the server
+    event.preventDefault();
+    console.log($('form').serialize());
+    $.ajax("/orders", {
+      method: "POST", 
+      data: $('#form').serialize(),
+      success: pizzaOrderSubmitted,
+        //do stuff with server response
+      failure: function(error){
+        //handle error
+      console.log(error);
+      }
+    });
+  });
 });
